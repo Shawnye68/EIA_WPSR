e8a060f2ec7ea3c6ffb241b7d4744be35a6e2f00
Shawnye68 <shawnye68@gmail.com>
2024-08-28 20:23:58 +0800

    Update eia_week_plot.py

--- eia_week_plot.py
+++ eia_week_plot.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
 
-wpsr = pd.read_csv('wpsr.csv')
+wpsr = pd.read_csv('WPSR.csv')
 p_st = pd.read_excel('psw01.xlsx', sheet_name = 'Data 1')
 update = pd.to_datetime(p_st.iloc[:,0], errors = 'coerce').dt.strftime('%Y-%m-%d').iloc[-1]
 st.set_page_config(layout="wide")
